6fdd7e4435076db1983ae9e727d9c1b9d03a4f0e
K. Townsend <kevin@adafruit.com>
2012-10-25 09:07:21 +0200

    First commit

---
+++ examples/Test/Test.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <Adafruit_LSM303.h>
+
+Adafruit_LSM303 lsm;
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+  // Make sure I2C is properly setup
+  Wire.begin();
+
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!lsm.init())
+  {
+    Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
+    while (1);
+  }
+}
+
+void loop() 
+{
+  lsm.read();
+  Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)lsm.accelData.y);       Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)lsm.accelData.z);     Serial.print(" ");
+  Serial.print("Mag X: "); Serial.print((int)lsm.magData.x);     Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)lsm.magData.y);         Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)lsm.magData.z);       Serial.print(" ");
+  delay(1000);
+}
